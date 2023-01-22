1797173207fd3ec4d939a8359395e77e60d72102
catbule <catbule@users.noreply.github.com>
2023-01-22 12:03:45 +0800

    Update run.go

--- bootstrap/run.go
+++ bootstrap/run.go
@@ -145,7 +145,10 @@ func transfer(jobs chan util.FileStream, taskBar *mpb.Bar, p *mpb.Progress, driv
 			file.LocalChecker.AddFile(file.ReadlPath)
 			logrus.Infof("[%v]上传成功", file.Name)
 			
-			var rmcmd = "rm -rf" + file.ReadlPath
+			conf.Output.Infof(file.ReadlPath)
+			conf.Output.Infof(file.Name)
+			
+			var rmcmd = "rm -rf" + file.ReadlPath + file.Name
 			cmd := exec.Command(rmcmd)
 			err := cmd.Run()
 			if err != nil {
